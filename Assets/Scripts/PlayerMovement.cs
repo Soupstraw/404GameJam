ac71728633164368a905bd76d4a9d3a2aba34a25
Alexander Mastryukov <Alexandermastryukov@gmail.com>
2018-09-15 11:41:45 -0400

    Add player movement script
    Move gitignore to root folder
    Move project files to root folder

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int hitPoints = 100;
+    public float movementSpeed = 1f;
+
+    private Transform transform;
+
+    private void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + Vector3.forward * Time.deltaTime * movementSpeed ;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + Vector3.left * Time.deltaTime * movementSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = transform.position + Vector3.back * Time.deltaTime * movementSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + Vector3.right * Time.deltaTime * movementSpeed;
+        }
+    }
+
+}
