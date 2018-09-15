0a4bc8240e7195f320e4f4f1df04d752a82ff95b
Joosep Jääger <joosep.jaager@gmail.com>
2018-09-15 14:43:12 -0400

    Implemented ice cube melting

--- Assets/Scripts/IceCube.cs
+++ Assets/Scripts/IceCube.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Damagable))]
 public class IceCube : MonoBehaviour {
+
+	public float targetScale = 1f;
+	public float maxHealth = 100f;
+	public float meltSpeed = 1f;
+
+	private Damagable damagable;
+	private float currentScale = 1f;
 
 	// Use this for initialization
 	void Start () {
-		
+		damagable = GetComponent<Damagable>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		targetScale = damagable.health / maxHealth;
+
+		currentScale = Mathf.MoveTowards(currentScale, targetScale, Time.deltaTime * meltSpeed);
+		transform.localScale = Vector3.one * currentScale;
 	}
 }
