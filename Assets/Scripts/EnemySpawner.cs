4070da7e74b38c862c16d3457d95eb2833e2d497
Alexander Mastryukov <Alexandermastryukov@gmail.com>
2018-09-15 15:03:37 -0400

    Add enemy spawners

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyContainer;
+    public GameObject iceCubePrefab;
+
+    public float spawnDelay = 3f;
+    public float spawnDelayVariance = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        while(true)
+        {
+            // create a new ice cube
+            GameObject newEnemy = GameObject.Instantiate(iceCubePrefab, transform.position, transform.rotation);
+            newEnemy.transform.SetParent(enemyContainer.transform);
+
+            yield return new WaitForSeconds(spawnDelay + Random.Range(-spawnDelayVariance, spawnDelayVariance));
+        }
+    }
+}
