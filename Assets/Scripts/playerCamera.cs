6026f8dc279987d7d0db8269caa9599ec33faefc
iamyuchy <iamyuchy@gmail.com>
2018-09-15 12:36:35 -0400

    camera script

---
+++ Assets/Scripts/playerCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCamera : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+    public float smoothing = 5f; 
+
+	// Use this for initialization
+	void Start () {
+        // Calculate the initial offset.
+        offset = transform.position - player.transform.position;
+	}
+	
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+        Vector3 targetCamPos = transform.position;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
